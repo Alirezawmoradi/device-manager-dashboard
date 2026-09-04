fdb7835a33e7ae7540be6610b8ebb4bfbbe66d4d
Alirezawmoradi <alirezawmoradi@gmail.com>
2026-09-04 11:45:21 +0330

    intial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex w-full min-h-screen text-gray-900 bg-white items-center justify-center">
+      Device manager Dashboard
+    </div>
+  );
+}
